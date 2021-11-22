3bd42833d04d1b5e8211f409c58a58cd35b1ad31
Tchang3 <tom_chang04@hotmail.fr>
2021-11-22 23:45:38 +0100

    Added movies.json and MovieList component in progress

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,18 @@
 import '../styles/MovieList.css'
+import moviesData from '../datas/movies.json'
 
 function MovieList() {
+  const moviesList = moviesData.results
   return (
-    <div>
-      
-    </div>
+    <ul className="movie-list">
+    {
+        moviesList.map((movie)=>(
+            <li className="movie-element">
+                <span className="movie-title">{movie.title}</span>
+            </li>
+        ))
+    }
+    </ul>
   )
 }
 
